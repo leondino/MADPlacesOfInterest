d564396a1fc2ee8c68ebc3abbbfc7eac0e63a381
Léon Imanuel <leondino0309@gmail.com>
2020-02-25 00:54:03 +0100

    Added the Adapter and place item layout

---
+++ app/src/main/java/com/example/placesofinterest/PlaceAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.placesofinterest
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_place.view.*
+
+class PlaceAdapter(private val places: List<Place>) : RecyclerView.Adapter<PlaceAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_place, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return places.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(places[position])
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        fun bind(place: Place){
+            itemView.placeText.text = place.name
+            itemView.placeImage.setImageResource(place.imageResId)
+        }
+
+    }
+}
