8b10cb41c2e940a1db24b5316eb498877d3c13f4
Léon Imanuel <leondino0309@gmail.com>
2020-02-25 01:46:49 +0100

    Wrote PlacesActivity to show connect everything and messed with the contraints to get things working like the example

--- app/src/main/java/com/example/placesofinterest/PlacesActivity.kt
+++ app/src/main/java/com/example/placesofinterest/PlacesActivity.kt
@@ -2,11 +2,29 @@ package com.example.placesofinterest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import kotlinx.android.synthetic.main.activity_places.*
 
 class PlacesActivity : AppCompatActivity() {
+
+    private var places = arrayListOf<Place>()
+    private var placeAdapter = PlaceAdapter(places)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_places)
+
+        initViews()
+    }
+
+    private fun initViews(){
+        rvPlaces.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        rvPlaces.adapter = placeAdapter
+        for (iPlace in Place.PLACE_NAMES.indices) {
+            places.add(Place(Place.PLACE_NAMES[iPlace], Place.PLACE_RES_DRAWABLE_IDS[iPlace]))
+        }
+        placeAdapter.notifyDataSetChanged()
     }
 }
